a1303dc9b04ecee9a13da740ae764694fd1f4dc4
Vo Tran Nha Linh <vtrnnhlinh@outlook.com>
2022-12-13 15:29:06 +0700

    add maximum and opposite

---
+++ 7kyu/maximumMultiple.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5aba780a6a176b029800041c
+
+int maxMultiple(int divisor, int bound) 
+{
+  // Your Code is Here ... Enjoy !!! 
+  int n = (int) bound/divisor;
+  return n*divisor;
+}
---
+++ 8kyu/oppositeNumber.cpp
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+int opposite(int number) 
+{
+  return -number;
+}
